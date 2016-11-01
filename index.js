01bd11acc4122edfff508c5a9bcf27ba57bdd921
Rahul Jaswa <rahul.jaswa@gmail.com>
2016-11-01 16:21:01 -0700

    Redirect HTTP requests to HTTPS

--- index.js
+++ index.js
@@ -3,15 +3,24 @@
 var express = require('express');
 var app = express();
 
-app.use(require('prerender-node').set('prerenderToken', process.env.PRERENDER_TOKEN));
+var localhostPort = 8000;
 
-var port = process.env.PORT || 8000;
+app.use(function(req, res, next) {
+	if ((req.headers['x-forwarded-proto'] != 'https') && (req.headers.host != ('localhost:' + localhostPort))) {
+		return res.redirect(['https://', req.get('Host'), req.url].join(''));
+	}
+	next();
+});
+
+app.use(require('prerender-node').set('prerenderToken', process.env.PRERENDER_TOKEN));
 
 app.use(express.static(__dirname + '/app'));
 
 app.all('/*', function(req, res) {
 	res.sendfile('app/index.html');
 });
+
+var port = process.env.PORT || localhostPort;
 
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
